380e6b93ee169d8bf2dd84de3af1151d7d962135
GharatShruti <shrutigharat62@gmail.com>
2024-04-07 23:01:22 +0530

    msg

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,21 +17,9 @@ export default function () {
             <Link className="nav-link active" aria-current="page" to="#">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="#">Features</Link>
+            <Link className="nav-link" to="/login">Login</Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="#">Pricing</Link>
-          </li>
-          <li className="nav-item dropdown">
-            <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown link
-            </Link>
-            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><Link className="dropdown-item" to="#">Action</Link></li>
-              <li><Link className="dropdown-item" to="#">Another action</Link></li>
-              <li><Link className="dropdown-item" to="#">Something else here</Link></li>
-            </ul>
-          </li>
+
         </ul>
       </div>
     </div>
