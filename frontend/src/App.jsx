c7060e63ebfc9b62b3f1f427134bb0d1b90c7f46
GharatShruti <shrutigharat62@gmail.com>
2024-04-07 22:32:11 +0530

    msg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
 import './App.css'
 import Home from './screens/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
  
 
   return (
-    <>
-    <div><Home /></div>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+      </Routes>
 
-    </>
+
+    </div>
+
+    </Router>
   )
 }
 
