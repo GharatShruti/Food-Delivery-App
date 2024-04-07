bf9d0c3339ff32ca4917bbbe1c8df65b7e261003
GharatShruti <shrutigharat62@gmail.com>
2024-04-07 23:13:33 +0530

    msg

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function () {
     <>
     <div><nav className="navbar navbar-expand-lg navbar-dark bg-success">
     <div className="container-fluid">
-      <Link className="navbar-brand" to="/">Navbar</Link>
+      <Link className="navbar-brand fs-1 fst-italic" to="/">Food Express</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
