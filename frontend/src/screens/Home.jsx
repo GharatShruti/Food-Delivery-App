df970c1dd020d7e9bd5e1f1d5991efecab7788ea
GharatShruti <shrutigharat62@gmail.com>
2024-04-08 23:15:12 +0530

    msg

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
-    <div>
-    <div> <Navbar/></div>
-    <div>body</div>
-    <div><Footer /></div>
+      <div>
+        <div>
+          <Navbar />
+        </div>
 
-    </div>
+        <div>
+          <div className="card mt-3" style={{ width: "18rem", "maxHeight":"360PX"}}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                This is some important text.
+              </p>
+              <div className="container"> </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </div>
     </>
-  )
+  );
 }
