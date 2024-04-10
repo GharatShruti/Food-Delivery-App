15b48c2e38d742794f708becacfc7354f3bdaa6c
GharatShruti <shrutigharat62@gmail.com>
2024-04-10 21:01:27 +0530

    msg

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousal from "../components/Carousal";
 
 export default function Home() {
   return (
@@ -8,33 +10,11 @@ export default function Home() {
         <div>
           <Navbar />
         </div>
-
+        <div><Carousal/></div>
         <div>
-          <div className="card mt-3" style={{ width: "18rem", "maxHeight":"360PX"}}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is some important text.
-              </p>
-              <div className="container w-100">
-                <select className="m-2 h-100 bg-success rounded">
-                  {Array.from(Array(6),(e,i)=>{
-                    return (
-                    <option key={i+1} value={i+1}>{i+1}</option>
-                    )
-                  })}</select>
-                  
-                  <select className="m-2 h-100 bg-success rounded">
-                    <option value="half">Half</option>
-                    <option value="full">Full</option>
-
-                  </select>
-
-                   </div>
-            </div>
-          </div>
+          <Card />
         </div>
+
         <div>
           <Footer />
         </div>
