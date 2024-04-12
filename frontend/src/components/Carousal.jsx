6e90ee256e5f41d5a5d502c418e743339b288527
GharatShruti <shrutigharat62@gmail.com>
2024-04-12 22:22:46 +0530

    msg

--- frontend/src/components/Carousal.jsx
+++ frontend/src/components/Carousal.jsx
@@ -26,10 +26,7 @@ export default function Carousal() {
     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Previous</span>
   </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
+
 </div>
         </div>
 
