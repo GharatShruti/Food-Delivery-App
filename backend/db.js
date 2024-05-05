41250d4f0c7f4f2ffbf40776ca902efa858d9f3c
GharatShruti <shrutigharat62@gmail.com>
2024-05-05 23:05:14 +0530

    msg

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/foodexpress';
 
-
-
+const mongoDB = () => {
+    mongoose.connect(mongoURI)
+        .then(() => {
+            console.log('Connected to MongoDB');
+        })
+        .catch((error) => {
+            console.error('Error connecting to MongoDB:', error);
+        });
+};
 
 module.exports = mongoDB;
