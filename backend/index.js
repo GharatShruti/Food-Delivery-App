05ce70e999f83e8f66d58ca5f24e8a9942daa1c1
GharatShruti <shrutigharat62@gmail.com>
2024-05-05 23:04:38 +0530

    msg

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
-// const mongoDB = require("./db")
-// const mongoDB = () => {
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/foodexpress';
-    mongoose.connect(mongoURI)
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch((error) => {
-            console.error('Error connecting to MongoDB:', error);
-        });
-// };
+const mongoDB = require("./db")
+// mongoDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
