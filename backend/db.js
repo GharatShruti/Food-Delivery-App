246ba724e54ac94cbc6dcfea8e644070ca92158f
GharatShruti <shrutigharat62@gmail.com>
2024-05-06 22:56:24 +0530

    msg

--- backend/db.js
+++ backend/db.js
@@ -4,18 +4,13 @@ const mongoURI = 'mongodb://localhost:27017/foodexpress';
 const mongoDB = async() => {
     await mongoose.connect(mongoURI,{useNewURIParser:true},async(err,result)=>{
         if(err){
-            console.log(err);
+            console.log("---",err);
         }
         else{
             console.log('Connected to MongoDB');
         }
     });
-        .then(() => {
-            console.log('Connected to MongoDB');
-        })
-        .catch((error) => {
-            console.error('Error connecting to MongoDB:', error);
-        });
+        
 };
 
 module.exports = mongoDB;
